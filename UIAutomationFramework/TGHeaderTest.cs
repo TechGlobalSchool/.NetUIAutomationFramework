5e7b198b541158c43872cf76c180af023c4fbf09
TechGlobalSchool <tgschool01@gmail.com>
2023-05-10 19:35:12 -0500

    first commit

---
+++ UIAutomationFramework/TGHeaderTest.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UIAutomationFramework;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    public void validateTGLogo()
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+
+        driver.Navigate().GoToUrl("https://techglobal-training.com/");
+
+        IWebElement logo = driver.FindElement(By.Id("logo"));
+
+        Assert.True(logo.Displayed);
+
+        Thread.Sleep(5000);
+
+        driver.Quit();
+    }
+
+
+    [Test]
+    public void validateTGHeader()
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+
+        driver.Navigate().GoToUrl("https://techglobal-training.com/");
+
+        IWebElement heading = driver.FindElement(By.CssSelector(".HomePage_pageTitle__UAMbk"));
+
+        Assert.True(heading.Displayed);
+        Assert.That(heading.Text.Equals("Welcome to TechGlobal School\nTraining"));
+
+        Thread.Sleep(5000);
+
+        driver.Quit();
+    }
+}
